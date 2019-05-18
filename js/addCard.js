d3b6041bdec71c7ad4fbfbbe2310e99c50555776
Gary Alan Ridgway <gary-ridgway@uiowa.edu>
2019-05-18 14:19:26 -0500

    early and often

--- js/addCard.js
+++ js/addCard.js
@@ -1,5 +1,67 @@
 function addCard() {
+	console.log('trigger');
 	$('#cardzone').append(
-		'<div class="card"></div>'
+		'<div class="card">\
+			<div class="card-top">\
+				<img class="decorative-forest-background" src="assets/CREATURES - BACKGROUND ART BEASTS.png" alt="Decorative Forest Background">\
+				<div class="name-plate">\
+					<p class="creature-specs" contenteditable="true">specifications</p>\
+					<img src="assets/CREATURES - NAMECARD.png" alt="Name Ribbon">\
+					<h2 class="name-text" contenteditable="true">name</h2>\
+				</div>\
+				\
+				<div class="stat-bar">\
+					<div class="outer-stat-box">\
+				  		<div class="stat-box-title">\
+					    	<p>skl</p>\
+					    </div>\
+					  	<div class="inner-stat-box">\
+					    	<p>#</p>\
+					  	</div>\
+					</div>\
+					<div class="outer-stat-box">\
+				  		<div class="stat-box-title">\
+					    	<p>skl</p>\
+					    </div>\
+					  	<div class="inner-stat-box">\
+					    	<p>#</p>\
+					  	</div>\
+					</div>\
+					<div class="outer-stat-box">\
+				  		<div class="stat-box-title">\
+					    	<p>skl</p>\
+					    </div>\
+					  	<div class="inner-stat-box">\
+					    	<p>#</p>\
+					  	</div>\
+					</div>\
+					<div class="outer-stat-box">\
+				  		<div class="stat-box-title">\
+					    	<p>skl</p>\
+					    </div>\
+					  	<div class="inner-stat-box">\
+					    	<p>#</p>\
+					  	</div>\
+					</div>\
+					<div class="outer-stat-box">\
+				  		<div class="stat-box-title">\
+					    	<p>skl</p>\
+					    </div>\
+					  	<div class="inner-stat-box">\
+					    	<p>#</p>\
+					  	</div>\
+					</div>\
+					<div class="outer-stat-box">\
+				  		<div class="stat-box-title">\
+					    	<p>skl</p>\
+					    </div>\
+					  	<div class="inner-stat-box">\
+					    	<p>#</p>\
+					  	</div>\
+					</div>\
+				</div>\
+			</div>\
+			<div class="card-bottom"></div>\
+		</div>'
 	);
 }
