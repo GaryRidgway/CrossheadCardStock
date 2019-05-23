0457cd6fa8974291bede96cd69801d6019e41561
Gary Alan Ridgway <gary-ridgway@uiowa.edu>
2019-05-23 13:10:31 -0500

    testing image adding

--- js/templates/addCard.js
+++ js/templates/addCard.js
@@ -3,13 +3,14 @@ function addCard(selector) {
 		'<div class="card" cardID="card-' + cardIDS + '">\
 			<div class="card-inner">\
 				<div class="card-top">\
-					<img class="decorative-forest-background" src="assets/CREATURES - BACKGROUND ART BEASTS.png" alt="Decorative Forest Background">\
 					<div class="name-plate">\
 						<p class="creature-specs" spellcheck="false" contenteditable="true">specifications</p>\
 						<img class="name-ribbon" src="assets/CREATURES - NAMECARD.png" alt="Name Ribbon">\
 						<h2 class="name-text" spellcheck="false" contenteditable="true">name</h2>\
 						<div class="cr-bar"></div>\
 					</div>\
+					<img class="decorative-forest-background" src="assets/CREATURES - BACKGROUND ART BEASTS.png" alt="Decorative Forest Background">\
+					<div class="creature-block"></div>\
 					<div class="stat-bar"></div>\
 					<div class="side-stats"></div>\
 				</div>\
@@ -20,7 +21,8 @@ function addCard(selector) {
 	
 	addStats(selector.find('.stat-bar'));
 	addCRBar(selector.find('.cr-bar'));
-	addSideStats(selector.find('.side-stats'))
+	addSideStats(selector.find('.side-stats'));
+	addCreature(selector.find('.creature-block'));
 
 	cardIDS++;
 }
