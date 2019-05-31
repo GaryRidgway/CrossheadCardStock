5ef8b5a774ae02a0de0923433f245cdcbba2f690
Gary Alan Ridgway <gary-ridgway@uiowa.edu>
2019-05-30 20:30:16 -0500

    updates on controls

--- js/templates/addCreature.js
+++ js/templates/addCreature.js
@@ -3,10 +3,18 @@ function addCreature(selector) {
 		'<div class="creature">\
 			<img class="creature-image" src="https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/c9edf76c-c971-445d-bbc1-6be12d18dc60/d37e05d-931ea46a-eff9-4505-a5f5-cea016341a72.png/v1/fill/w_400,h_371,strp/pikachu_by_moni158_d37e05d-fullview.png?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7ImhlaWdodCI6Ijw9MzcxIiwicGF0aCI6IlwvZlwvYzllZGY3NmMtYzk3MS00NDVkLWJiYzEtNmJlMTJkMThkYzYwXC9kMzdlMDVkLTkzMWVhNDZhLWVmZjktNDUwNS1hNWY1LWNlYTAxNjM0MWE3Mi5wbmciLCJ3aWR0aCI6Ijw9NDAwIn1dXSwiYXVkIjpbInVybjpzZXJ2aWNlOmltYWdlLm9wZXJhdGlvbnMiXX0.9aNKhv7iPG6vHawnpW9OkKCKGyDwAjfhhpm-1b403AA" alt="creature-image">\
 		</div>\
-		<div class="creature-controls">\
-			<div class="rotate-creature"></div>\
-			<div class="slidecontainer">\
-			  <input type="range" min="-100" max="100" value="0" class="slider horizontal-position">\
+		<div class="creature-controls-container">\
+			<div class="creature-controls">\
+				<div class="rotate-creature"></div>\
+				<div class="slidecontainer">\
+					<div class="horizontal">\
+						<div class="horizontal-position-header">\
+							<div class="horizontal-position-title">Horizontal</div>\
+							<input type="number" class="slider-horizontal-tooltip" min="-100" max="100" value="0">\
+						</div>\
+					  	<input type="range" min="-100" max="100" value="0" class="slider horizontal-position">\
+					</div>\
+				</div>\
 			</div>\
 		</div>\
 		'
